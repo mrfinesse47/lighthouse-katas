87c8aca4ea2711d5ac62f137358084546d2ec529
Kevin Mason <krmason2020@gmail.com>
2021-10-11 02:56:28 +0000

    completed camel case kata

---
+++ case-maker.js
@@ -0,0 +1,26 @@
+const camelCase = function (input) {
+  const inputArray = [...input]; //creates an array we will do this C style
+  let resultString;
+  for (i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === " ") {
+      //-32
+      // ch.codePointAt(pos)
+
+      inputArray[i + 1] = String.fromCharCode(
+        inputArray[i + 1].codePointAt(0) - 32
+      );
+    }
+  }
+
+  resultString = inputArray.toString();
+  resultString = resultString.replace(/,/gi, "");
+  return resultString.replace(/ /gi, "");
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
+
+// thisIsAString
+// loopyLighthouse
+// supercalifragalisticexpialidocious
