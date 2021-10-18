43710c5d60921c8cac2747255c304b3e300df34e
Kevin Mason <krmason2020@gmail.com>
2021-10-18 04:54:36 +0000

    completed case maker II kata

--- case-maker-II.js
+++ case-maker-II.js
@@ -1,6 +1,87 @@
-const makeCase = function(input, case) {
-  // Put your solution here
-}
+const makeCase = function (input, caseType) {
+  let resultStr = input;
+
+  typeof caseType === "object"
+    ? caseType.forEach((c) => {
+        resultStr = convertCase(resultStr, c);
+      })
+    : (resultStr = convertCase(input, caseType));
+  return resultStr;
+};
+
+const camel = (input) => {
+  let res = removeSpaces(firstEveryLetterToUpper(input));
+  return res[0].toLowerCase() + res.slice(1);
+};
+
+const pascal = (input) => {
+  return removeSpaces(firstEveryLetterToUpper(input));
+};
+
+const snake = (input) => {
+  return input.replace(/\s/g, "_");
+};
+
+const kebab = (input) => {
+  return input.replace(/\s/g, "-");
+};
+
+const title = (input) => {
+  return firstEveryLetterToUpper(input);
+};
+
+const vowel = (input) => {
+  return input.replace(/[aeiouy]/g, (vowel) => vowel.toUpperCase());
+};
+
+const consonant = (input) => {
+  return input.replace(/[^aeiouy]/g, (cons) => cons.toUpperCase());
+};
+
+//helper functions-------------------------------------------------//
+
+const firstEveryLetterToUpper = (input) => {
+  return input.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+    letter.toUpperCase()
+  );
+};
+
+const removeSpaces = (input) => {
+  return input.replace(/\s/g, "");
+};
+
+const convertCase = (input, caseType) => {
+  let resultStr = "";
+  switch (caseType) {
+    case "camel":
+      resultStr = camel(input);
+      break;
+    case "pascal":
+      resultStr = pascal(input);
+      break;
+    case "snake":
+      resultStr = snake(input);
+      break;
+    case "kebab":
+      resultStr = kebab(input);
+      break;
+    case "title":
+      resultStr = title(input);
+      break;
+    case "vowel":
+      resultStr = vowel(input);
+      break;
+    case "consonant":
+      resultStr = consonant(input);
+      break;
+    case "upper":
+      resultStr = input.toUpperCase();
+      break;
+    default:
+      resultStr = input;
+  }
+  return resultStr;
+};
 
 console.log(makeCase("this is a string", "camel"));
 console.log(makeCase("this is a string", "pascal"));
--- url-encoded-strings.js
+++ url-encoded-strings.js
@@ -1,30 +1,41 @@
+// const urlDecode = function (text) {
+//   text = text.replace(/%20/g, " "); //removing %20 replacing with space
+//   const arrayOfArgs = [];
+//   const resultObj = {};
+//   let lhs = "";
+//   let rhs = "";
+
+//   for (let i = 0; i < text.length; i++) {
+//     if (text[i] === "&") {
+//       //perhaps i could use the method below instead of iterating through the string, but this was fun too.
+//       arrayOfArgs.push(text.slice(0, i));
+//       text = text.slice(i + 1);
+
+//       i = -1; //essentially sets i to 0;
+//     }
+//   }
+//   arrayOfArgs.push(text);
+
+//   //now that we have all the args in an array we need to set it to an object
+
+//   arrayOfArgs.forEach((arg) => {
+//     lhs = arg.slice(0, arg.indexOf("=")); //could do this on one line but didn't, and decided to use in between variables for readability.
+//     rhs = arg.slice(arg.indexOf("=") + 1);
+//     resultObj[lhs] = rhs;
+//   });
+
+//   return resultObj;
+// };
+
 const urlDecode = function (text) {
-  text = text.replace(/%20/g, " "); //removing %20 replacing with space
-  const arrayOfArgs = [];
-  const resultObj = {};
-  let lhs = "";
-  let rhs = "";
-
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] === "&") {
-      //perhaps i could use the method below instead of iterating through the string, but this was fun too.
-      arrayOfArgs.push(text.slice(0, i));
-      text = text.slice(i + 1);
-
-      i = -1; //essentially sets i to 0;
+  let decoded = text.replace(/(?:%20)/g, " ").split(/(?:=|&)/g);
+  let objects = {};
+  for (let i = 0; i < decoded.length; i++) {
+    if (i % 2 === 0) {
+      objects[decoded[i]] = decoded[i + 1];
     }
   }
-  arrayOfArgs.push(text);
-
-  //now that we have all the args in an array we need to set it to an object
-
-  arrayOfArgs.forEach((arg) => {
-    lhs = arg.slice(0, arg.indexOf("=")); //could do this on one line but didn't, and decided to use in between variables for readability.
-    rhs = arg.slice(arg.indexOf("=") + 1);
-    resultObj[lhs] = rhs;
-  });
-
-  return resultObj;
+  return objects;
 };
 
 console.log(urlDecode("duck=rubber"));
