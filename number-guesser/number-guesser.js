2a555ce62b84226634dd13ddb43e65a2afa77237
Kevin Mason <krmason2020@gmail.com>
2021-10-19 00:23:29 +0000

    completed number-guesser.js

--- number-guesser/number-guesser.js
+++ number-guesser/number-guesser.js
@@ -1,5 +1,45 @@
 let prompt = require("prompt-sync")();
-
+let number = Math.floor(Math.random() * 100) + 1; //random number
+let guesses = 0;
+let answer;
+let answers = [];
 // code below (replace this example)
-let answer = prompt("Guess a number: ");
-console.log("You answered: " + answer);
+do {
+  answer = parseInt(prompt("Guess a number: "));
+
+  if (answer) {
+    if (!answers.includes(answer)) {
+      if (answer < number) {
+        console.log("Too low!");
+      }
+      if (answer > number) {
+        console.log("Too high!");
+      }
+
+      answers.push(answer);
+      guesses++;
+    } else {
+      console.log("Already Guessed!");
+    }
+  } else {
+    console.log("Not a number! Try again!");
+  }
+} while (answer !== number);
+
+console.log(`You got it! You took ${guesses} attempts!`);
+
+// Guess a number:
+// > 12
+// Too Low!
+// Guess a number:
+// > 65
+// Too High!
+// Guess a number:
+// > 65
+// Already Guessed!
+// Guess a number:
+// > asdf
+// Not a number! Try again!
+// Guess a number:
+// > 42
+// You got it! You took 3 attempts!
